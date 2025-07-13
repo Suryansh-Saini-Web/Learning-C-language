596f77a7affcbd20b294069ea455064ff3a831a5
Suryansh Saini <suryansh19saini@gmail.com>
2025-07-13 08:46:21 +0530

    Learning about C basics - Data types, scanf, printf

---
+++ 01_learning.c
@@ -0,0 +1,62 @@
+// -----------Learning about Boiler Plate code. (uncomment to execute)-------
+
+/*
+#include<stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
+*/
+
+
+// ----------Learning about data types. (uncomment to execute)--------------
+
+/*
+#include<stdio.h>
+
+int main(){
+    float height = 5.9;
+    int weight = 51;
+    char name = 'S';
+    printf("My name is %c \n", name );
+    printf("My weight is %d Kg \n", weight);    
+    printf("My height is %.1f feet", height);
+    return 0;
+}
+*/
+
+
+// -------Learning about calculation in C. (uncomment to execute)--------
+/*
+#include<stdio.h>
+
+int main(){
+    int section_A = 59;
+    int section_B = 47;
+    int section_C = 36;
+    int total_students = section_A + section_B + section_C;
+    printf("Total students in class 12th - %d", total_students);
+    return 0;
+}
+*/
+
+// ---------Learning about taking input from user. (uncomment to execute)-------
+
+/*
+#include<stdio.h>
+
+int main(){
+    char name;
+    float weight;
+    float height;
+    printf("Please enter first letter of your name \n");
+    scanf("%c", &name);
+    printf("Please enter your weight in Kg \n");
+    scanf("%f", &weight);
+    printf("Please enter your height in feet \n");
+    scanf("%f", &height);
+    printf("My name is %c, my height is %.1f feet and my weight is %.2f Kg", name, height, weight);
+    return 0;
+}
+*/
