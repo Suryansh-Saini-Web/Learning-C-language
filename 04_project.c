39f328056fa84031ac13dc48a7495d64e928f0e6
Suryansh Saini <suryansh19saini@gmail.com>
2025-09-18 20:09:21 +0530

    Updated 04_project.c

--- 04_project.c
+++ 04_project.c
@@ -1,54 +1,55 @@
 // --------------------------------------Creating a Number Guessing Game.----------------------------------------------
 
 #include<stdio.h>
-#include<stdlib.h>
 #include<time.h>
-#include<string.h>
+#include<stdlib.h>
 
 int main(){
-    char replay = 'y';
+    char play = 'y';
     srand(time(0));
-    while(replay == 'y' || replay == 'Y'){
+    while(play == 'y' || play == 'Y'){
         int rand_num = (rand() % 30) + 1;
-        int input;
         char buffer[100];
+        int user_input;
         int chances = 4;
-        int guesses[4];
+        int prev_guess[4];
         int guess_index = 0;
         while(chances){
             if(guess_index){
-                printf("Previous Guesses: ");
+                printf("Previous guesses:");
                 for(int i = 0; i < guess_index; i++){
-                    printf("%d ", guesses[i] );
+                    printf(" %d", prev_guess[i]);
                 }
                 printf("\n");
             }
-            printf("Chances left: %d\n", chances);
-            printf("Please enter a number between 1 and 30: ");
+            printf("Chances left: %d \n", chances);
+            printf("Please enter a number between 1 to 30: ");
             fgets(buffer, 100, stdin);
-            if(sscanf(buffer, "%d", &input) == 1 && input >= 1 && input <= 30){
-                guesses[guess_index++] = input;
-                if(input > rand_num){
-                    printf("You have entered a higher number.\n");
+            if(sscanf(buffer,"%d%1s", &user_input) == 1 && user_input > 0 && user_input < 31){
+                if(user_input > rand_num){
+                    printf("Please enter a lower number.\n");
+                    prev_guess[guess_index++] = user_input;
                     chances--;
-                }else if(input < rand_num){
-                    printf("You have entered a lower number.\n");
+                }else if(user_input < rand_num){
+                    printf("Please enter a higher number.\n");
+                    prev_guess[guess_index++] = user_input;
                     chances--;
                 }else{
-                    printf("You have won the Game.\n");
+                    printf("You have won the game.\n");
                     break;
                 }
             }else{
-                printf("Please enter a valid number.\n");
-            }       
+                printf("Please enter a valid input.\n");
+            }   
         }
-        if(chances == 0){
-            printf("You have lose the Game.\n");
-            printf("Correct number was: %d\n", rand_num);
-        }
-        printf("Do you want to play again?(y/n)\n");
-        scanf("%c", &replay);
-        getchar();
+        if(!chances){
+            printf("You have lost the game.\n");
+            printf("Correct number was: %d \n", rand_num);
+        }   
+        printf("Do you want to play again(y/n) \n");
+        scanf("%c", &play);
+        int c;
+        while((c = getchar()) != '\n' && c != EOF);
     }
     return 0;
 }
